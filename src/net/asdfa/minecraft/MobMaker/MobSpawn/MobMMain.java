fcd88c9ad292d03b43bf65e9579992476e46419a
astephens <SirIntellegence@users.noreply.github.com>
2011-06-24 12:17:33 -0600

    Working (Does not crash...)

---
+++ src/net/asdfa/minecraft/MobMaker/MobSpawn/MobMMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.asdfa.minecraft.MobMaker.MobSpawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author austin
+ */
+public class MobMMain extends JavaPlugin {
+    
+    public void onEnable(){
+	
+    }
+    
+    public void onDisable(){
+	
+    }
+}
